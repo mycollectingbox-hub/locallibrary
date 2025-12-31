694e9cc0cff0a6833befd31e4b820c337cc33594
hep_chen <hep_chen@iis.sinica.edu.tw>
2025-12-31 16:36:21 +0800

    Inline editing of associated records

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
+
 # Register your models here.
 from .models import Author, Genre, Book, BookInstance, Language
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
 
 # admin.site.register(Book)
 # admin.site.register(Author)
@@ -24,6 +29,8 @@ admin.site.register(Author, AuthorAdmin)
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
