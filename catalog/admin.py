b1f676fc032de27c9b8f053d969c4e7ae0f197c8
hep_chen <hep_chen@iis.sinica.edu.tw>
2025-12-31 16:18:42 +0800

    add "sections" to group related model information within the detail form, using the fieldsets attribute.

--- catalog/admin.py
+++ catalog/admin.py
@@ -32,3 +32,12 @@ class BookInstanceAdmin(admin.ModelAdmin):
     """
     list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
